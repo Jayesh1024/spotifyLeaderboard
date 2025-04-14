307d2e216554ff6bf79ac551926b347b2666e5d8
Jayesh <jayeshsmhaske1@gmail.com>
2025-04-15 00:41:40 +0530

    add retry decorator and implemented logging

--- api_test.py
+++ api_test.py
@@ -1,14 +1,48 @@
+from config import *
+import logging
 import os
+import time
 import requests
 from urllib import parse
 import json
 import dotenv
 dotenv.load_dotenv(override=True)
 
+logging.basicConfig(
+    level=LOG_LEVEL,
+    filename=LOG_FILE_PATH,
+    format=LOG_FORMAT
+)
+
 headers = {
     'Authorization': f'Bearer {os.environ["ACCESS_TOKEN"]}',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
 }
+
+def retry(max_retry=10,retry_interval=6):
+    def actual_decorator(func):
+        def wrapper(*args,**kwargs):
+            retry_count = 1
+            res = func(*args,**kwargs)
+            while res.status_code!=200:
+                logging.warning(f'Unsuccesful response, retrying, retry number:{retry_count}')
+                if retry_count>max_retry:
+                    error = {
+                        'code':res.status_code,
+                        'text':res.text,
+                        'reason':res.reason
+                    }
+                    logging.error(f'Unsuccesful response, max retries reached, error:{error}')
+                    raise requests.exceptions.RetryError(f"Unsuccesful response, max retries reached, error:{error}")
+
+                import time
+                time.sleep(retry_interval)
+                res = func(*args,**kwargs)
+                retry_count += 1
+            
+            return res
+        return wrapper
+    return actual_decorator
 
 def set_token_if_expired(func):
     def wrapper(*args,**kwargs):
@@ -37,17 +71,11 @@ def get_artist_albums():
         print(response.status_code)
         print(response.json())
 
-def get_artists():
-    url = ''
+@retry()
+def get_artist(artist_id):
+    url = f"https://api-partner.spotify.com/pathfinder/v1/query?operationName=queryArtistOverview&variables=%7B%22uri%22%3A%22spotify%3Aartist%3A{artist_id}%22%2C%22locale%22%3A%22%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221ac33ddab5d39a3a9c27802774e6d78b9405cc188c6f75aed007df2a32737c72%22%7D%7D"
     response = requests.get(url=url, headers=headers)
-    print(response.url)
-    if response.status_code == 200:
-        with open('token_cookies.json', 'w') as f:
-            json.dump(obj=response.json(), fp=f, indent=2)
-    else:
-        print(response.status_code)
-        print(response.text)
-    return
+    return response
 
 @set_token_if_expired
 def get_charts_playlists(): # gives all the Top 50 songs charts across multiple regions, but only the playlist level data and not it's contents.
@@ -93,12 +121,12 @@ def get_all_playlists(path_to_charts):
                 'code': response.status_code,
                 'body': response.json()
             }
-            print(error)
+            logging.error(f"Unsuccessful response, error:{error}")
     
     with open('playlist_data.json','w') as f:
         json.dump(response_playlist,f,indent=2)
 
-def retrieve_artists(path_to_playlist_data):
+def retrieve_artists_list(path_to_playlist_data): # Retrieves artist id and name as key:value and saves it to json file artists.json
     with open(path_to_playlist_data,'r') as f:
         playlists = json.load(f)
     artist_ids = {}
@@ -111,6 +139,29 @@ def retrieve_artists(path_to_playlist_data):
     with open('artists.json','w') as f:
         json.dump(artist_ids,f,indent=2)
 
-retrieve_artists('playlist_data.json')
-        
+def retrieve_artists(path_to_artists_list):
+    with open(path_to_artists_list,'r') as f:
+        artists = json.load(f)
+    
+    
+    res = []
+    for i,(artist_id,_) in enumerate(artists.items()):
+        #Batching logic
+        if i!=0 and i%100 == 0:
+            logging.info(f"Batch size 100, Batch number {i} complete, Batch writing to disk")
+            with open('artist_data.json', 'w') as f:
+                json.dump(res,f,indent=2)
 
+
+        logging.info(f"Iteration number {i}, Batch number {(i//100)+1}")
+        response = get_artist(artist_id)
+
+        if response.status_code == 200:
+            res.append(response.json())
+
+
+        
+    
+
+
+retrieve_artists('artists.json')
---
+++ config.py
@@ -0,0 +1,3 @@
+LOG_LEVEL = "INFO"
+LOG_FILE_PATH = "./pipeline.log"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - Line: %(lineno)d - %(message)s"
